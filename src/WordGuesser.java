ef6eccc4ba025be8c792b455846b6d439a942d54
mcdebarros <mauricio.debarros@ucalgary.ca>
2024-02-06 20:05:54 -0700

    Working code; some changes left (maybe?)

--- src/WordGuesser.java
+++ src/WordGuesser.java
@@ -1,154 +1,269 @@
+//Import useful packages
 import java.util.Scanner;
 import java.util.Arrays;
+import java.util.HashSet;
 
+//Define wordGuesser class
 public class WordGuesser {
 
-    // TODO: Add javadocs
-
-    // Maximum number of guesses; constant
+    //Define maximum allowable guesses and some dialogue strings to reduce downstream clutter
     static final int MAXGUESSES = 6;
+    static final String HELPMESSAGE = """
+            Wordle is a word guessing game! A secret word has been chosen, and it's your job to guess it.
+            The secret word will always have exactly six letters, and you have exactly six guesses to figure it out.
+            After each guess, a "game board" will appear in the console. For each guess you input, a new row will appear on the board.
+            Each row will contain six numbers. Each number in that row represents a letter in your guess.
+            "0" means that the letter in this position in your guess is not in the secret word.
+            "1" means that the letter in this position in your guess is in the secret word, but not in the correct position.
+            "2" means that the letter in this position exactly matches the secret word; don't change it!
+            Your score is calculated as the sum of the numbers in the final row of the game board
+            plus the amount of guesses remaining when the word was correctly guessed.
+            Remember, you can always access these instructions by entering the command "help".
+            If at any point you wish to stop playing, you can quit the game by entering the command "exit".
+            This implementation of Wordle allows for the user to set a secret word to solve for.
+            """;
+    static final String intro = """
+        Welcome to Wordle, the word guessing game!
+        If you'd like to learn how to play, just enter the command "help" at any point during gameplay.
+        If at any point you wish to quit the game, just enter the command "exit".
+        """;
+    static final String badMenuInput = """
+        I'm sorry, I didn't understand your input.
+        To set a word, please enter the command "Y".
+        To use the default word, please enter the command "N".
+        For instructions on how to play, please enter the command "help".
+        To quit the game, please enter the command "exit".
+        """;
+    static final String CUSTOMWORD = "Would you like to set your own secret word? (Y/N)";
+    static final String NOTLETTER = "Oops! Words must only contain letters!";
+    static final String BADLENGTH = "Oops! Words must be EXACTLY six letters long!";
+    static final String GOODBYE = "Play again soon!";
+    static final String YES = "Y";
+    static final String NO = "N";
+    static final String EXIT = "EXIT";
+    static final String HELP = "HELP";
+    static final String DEFAULT = "RANDOM";
+    static final String WINNER = "Congratulations, you've won! Would you like to play again? (Y/N)";
+    static final String LOSER = "Better luck next time! Would you like to play again? (Y/N)";
+    static final String YESNOERROR = "I'm sorry, I didn't understand your input. Please choose one of 'Y' or 'N'";
 
+    /**
+     * Retrieve guess or secret word from user via scanner
+     * @return word String; secret word or current guess
+     */
     public static String getWord() {
-        Scanner guess = new Scanner(System.in);
+        Scanner word = new Scanner(System.in);
         System.out.println("Type a six letter word:");
-        return guess.nextLine();
+        return word.nextLine();
     }
 
-    public static String fixCase(String guess) {
-        return guess.toUpperCase();
+    /**
+     * Solves case-dependency by setting all characters to capital letters
+     * @param word String; string object containing guess or secret word
+     * @return guess or secret word string object converted to uppercase
+     */
+    public static String fixCase(String word) {
+        return word.toUpperCase();
     }
 
-    public static boolean isLetter(String guess) {
+    /**
+     * Checks the guess for non-alphabetic characters
+     * @param word String; string object containing guess or secret word
+     * @return Boolean; true if string contains non-alphabetic characters; true otherwise
+     */
+    public static boolean isNotLetter(String word) {
 
         boolean allAlpha = true;
 
-        for (int j = 0; j < guess.length(); j++) {
-            if (!Character.isLetter(guess.charAt(j))) {
+        for (int j = 0; j < word.length(); j++) {
+            if (!Character.isLetter(word.charAt(j))) {
                 allAlpha = false;
                 break;
             }
         }
-        return allAlpha;
+        return !allAlpha;
     }
 
-    public static boolean guessLength(String guess) {
+    /**
+     * Checks if the guess or secret word is exactly six characters long
+     * @param word String; string object containing guess or secret word
+     * @return Boolean; true if string is not exactly six characters; false otherwise
+     */
+    public static boolean wordLength(String word) {
 
-        return guess.length() == 6;
+        return word.length() != 6;
     }
 
-    public static boolean checkExit(String guess) {
+    /**
+     * Creates a set of characters in the secret word
+     * @param word String; string object containing guess or secret word
+     * @return HashSet of characters in the secret word
+     */
+    public static HashSet<Character> wordContent(String word) {
 
-        return guess.equals("EXIT");
-    }
+        HashSet<Character> content = new HashSet<>();
 
-    public static String guessContent(String guess) {
-
-        String charToCheck;
-        StringBuilder guessContent = new StringBuilder();
-
-        for (int j = 0; j < guess.length(); j++) {
-            charToCheck = String.valueOf(guess.charAt(j));
-            if (!guessContent.toString().contains(charToCheck)) {
-                guessContent.append(charToCheck);
+        for (int j = 0; j < word.length(); j++) {
+            content.add(word.charAt(j));
             }
-        }
-        return guessContent.toString();
+        return content;
     }
 
-    public static boolean game() {
+    /**
+     * Execution of the wordle game
+     * @param secretWord String; string object containing guess or secret word
+     * @return Boolean; true if user wishes to play another game; false otherwise
+     */
+    public static boolean game(String secretWord) {
 
-        String secretWord = "RESUME";
+        if (secretWord.isEmpty()) {
+            secretWord = DEFAULT;}
         String guess;
-        String secretContent = guessContent(secretWord);
-        String guessContent;
-        String charToCheck;
+        HashSet<Character> secretContent = wordContent(secretWord);
         String playAgain;
-        String yes = "Y";
-        String no = "N";
+        char[] guessArr = new char[6];
+        char[] secretArr = new char[6];
+        int finalScore = 0;
         int numGuesses = 0;
         int correctPos = 0;
-        int correctLetter;
         int[][] board = new int[6][6];
-        boolean endGame = false;
-        boolean choice;
 
         Scanner newGame = new Scanner(System.in);
+        System.out.println("Cool! Let's play! \n");
+
+        for (int i = 0; i < 6; i++) {
+            secretArr[i] = secretWord.charAt(i);
+        }
 
         while ((numGuesses < MAXGUESSES) && (correctPos < 6)) {
 
-            correctLetter = 0;
             correctPos = 0;
             guess = fixCase(getWord());
-            guessContent = "";
-            if (!isLetter(guess)) {
-                System.out.println("Guess must only contain letters!");
+
+            if (guess.equals(HELP)) {
+                System.out.println(HELPMESSAGE);
                 continue;
-            } else if (checkExit(guess)) {
-                endGame = true;
-                break;
-            } else if (!guessLength(guess)) {
-                System.out.println("Guess must be EXACTLY six letters long!");
+            } else if (isNotLetter(guess)) {
+                System.out.println(NOTLETTER);
+                continue;
+            } else if (guess.equals(EXIT)) {
+                return false;
+            } else if (wordLength(guess)) {
+                System.out.println(BADLENGTH);
                 continue;
             } else {
 
-                for (int i = 0; i < secretWord.length(); i++) {
-                    charToCheck = String.valueOf(guess.charAt(i));
-                    if ((secretContent.contains(charToCheck)) && (!guessContent.contains(charToCheck))) {
-                        correctLetter++;
-                        guessContent += charToCheck;
-                    }
+                for (int i = 0 ; i < 6; i++) {
+                    guessArr[i] = guess.charAt(i);
                 }
-                for (int i = 0; i < guess.length(); i++) {
-                    charToCheck = String.valueOf(guess.charAt(i));
-                    if (guessContent.contains(charToCheck)) {
-                        board[numGuesses][i] += 1;
-                    }
-                    if (guess.charAt(i) == secretWord.charAt(i)) {
-                        board[numGuesses][i] += 1;
-                        correctPos +=1;
+                for (int i = 0; i < secretWord.length(); i++) {
+                    if (secretContent.contains(guessArr[i])) {
+                        board[numGuesses][i]++;
+                        if (secretArr[i] == (guessArr[i])) {
+                            board[numGuesses][i]++;
+                            correctPos++;
+                        }
                     }
                 }
             }
             for (int i = 0; i < numGuesses + 1; i++) {
-                System.out.println(Arrays.toString(board[numGuesses]));
+                System.out.println(Arrays.toString(board[i]));
             }
             numGuesses++;
+            if (correctPos != 6) {
+                System.out.println(STR."You have \{MAXGUESSES - numGuesses} guesses remaining!");
+            }
+        }
+        finalScore += (MAXGUESSES - numGuesses);
+        for (int i = 0; i < 6; i++) {
+            finalScore = finalScore + board[numGuesses - 1][i];
         }
         if (correctPos == 6) {
-            System.out.println("Congratualtions, you've won! Would you like to play again? (Y/N)");
+            System.out.println(WINNER);
+            System.out.println(STR."Your final score was \{finalScore} points!");
             playAgain = fixCase(newGame.nextLine());
-            while (!((playAgain.equals(no)) || (playAgain.equals(yes)))) {
-                System.out.println("I'm sorry, I didn't understand your input. Please choose one of 'Y' or 'N'");
-                playAgain = fixCase(newGame.nextLine());
+            while (!((playAgain.equals(NO)) || (playAgain.equals(YES)))) {
+                if (playAgain.equals(HELP)) {
+                    System.out.println(HELPMESSAGE);
+                    System.out.println(WINNER);
+                    System.out.println(STR."Your final score was \{finalScore} points!");
+                    playAgain = fixCase(newGame.nextLine());
+                } else if (playAgain.equals(EXIT)) {
+                    System.out.println(GOODBYE);
+                    return false;
+                } else {
+                    System.out.println(YESNOERROR);
+                    System.out.println(WINNER);
+                    System.out.println(STR."Your final score was \{finalScore} points!");
+                    playAgain = fixCase(newGame.nextLine());
+                }
             }
-            return playAgain.equals(yes);
-        } else if (endGame) {
-            System.out.println("Play again soon!");
-            return false;
         } else {
-            System.out.println("Better luck next time! Would you like to play again? (Y/N)");
+            System.out.println(LOSER);
+            System.out.println(STR."Your final score was \{finalScore} points!");
             playAgain = fixCase(newGame.nextLine());
-            while (!((playAgain.equals(no)) || (playAgain.equals(yes)))) {
-                System.out.println("I'm sorry, I didn't understand your input. Please choose one of 'Y' or 'N'");
-                playAgain = fixCase(newGame.nextLine());
+            while (!((playAgain.equals(NO)) || (playAgain.equals(YES)))) {
+                if (playAgain.equals(HELP)) {
+                    System.out.println(HELPMESSAGE);
+                    System.out.println(LOSER);
+                    System.out.println(STR."Your final score was \{finalScore} points!");
+                    playAgain = fixCase(newGame.nextLine());
+                } else if (playAgain.equals(EXIT)) {
+                    System.out.println(GOODBYE);
+                    return false;
+                } else {
+                    System.out.println(YESNOERROR);
+                    System.out.println(LOSER);
+                    System.out.println(STR."Your final score was \{finalScore} points!");
+                    playAgain = fixCase(newGame.nextLine());
+                }
             }
-            return playAgain.equals(yes);
         }
+        return playAgain.equals(YES);
     }
 
     public static void main(String[] args) {
-        System.out.println();
+        Scanner mainMenu = new Scanner(System.in);
+        String input;
+        String secretWord;
+
+        System.out.println(intro);
+
         boolean play = true;
+        label:
         while (play) {
-            play = game();
+            System.out.println(CUSTOMWORD);
+            input = fixCase(mainMenu.nextLine());
+            while (!((input.equals(YES)) || (input.equals(NO)) || (input.equals(EXIT)) || (input.equals(HELP)))) {
+                System.out.println(badMenuInput);
+                input = fixCase(mainMenu.nextLine());}
+            switch (input) {
+                case HELP:
+                    System.out.println(HELPMESSAGE);
+                    break;
+                case EXIT:
+                    System.out.println(GOODBYE);
+                    break label;
+                case NO:
+                    secretWord = "";
+                    play = game(secretWord);
+                    break;
+                default:
+                    secretWord = fixCase(getWord());
+                    if (secretWord.equals(HELP)) {
+                        System.out.println(HELPMESSAGE);
+                    } else if (secretWord.equals(EXIT)) {
+                        System.out.println(GOODBYE);
+                        break label;
+                    } else if (wordLength(secretWord)) {
+                        System.out.print(BADLENGTH);
+                    } else if (isNotLetter(secretWord)) {
+                        System.out.println(NOTLETTER);
+                    } else {
+                        play = game(secretWord);
+                    }
+                    break;
+            }
         }
     }
 }
-
-
-
-    // TODO: Fully implement these checks, call them in main(), and create proper javadocs for all of them
-
-
-        // do I need this? Take it out if I don't, I guess
-        //boolean checkIsWord(String toCheck)
